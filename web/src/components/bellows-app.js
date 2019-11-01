1fdc79bd350220d05e00b4e1fb5af25be74d6fcf
Benji Fischman <befischm@microsoft.com>
2019-11-01 14:36:56 -0700

    Adding licensing headers to all js files.

--- web/src/components/bellows-app.js
+++ web/src/components/bellows-app.js
@@ -1,3 +1,10 @@
+  
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
 const BaseViewElement = require("./base-view");
 const { Providers, MsalProvider, TeamsProvider } = require("@microsoft/mgt");
 const teams = require("@microsoft/teams-js/dist/MicrosoftTeams");
